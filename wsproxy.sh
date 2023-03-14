b7650d3e30fd3ace051fcf03417b81d7facc1907
docdnp <contactme@via-github.please>
2023-03-14 12:20:47 +0100

    moved wsproxy to docker curl

--- wsproxy.sh
+++ wsproxy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --rm -v $PWD/nginx.conf:/etc/nginx/conf.d/default.conf --name nginx -p 80:80 nginx:latest
+docker run --rm -v $PWD/nginx.conf:/etc/nginx/conf.d/default.conf --network host --name nginx -p 80:80 nginx:latest
+
+docker ps | grep -Eo 'wsclient.*'  | xargs docker kill
+
